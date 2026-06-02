256463121bc07ebeeac9b1669f4733fc2b144159
Konstantinos Priftis <priftis.kostas1997@gmail.com>
2026-06-02 16:56:20 +0300

    Type hint in example views

--- views.py
+++ views.py
@@ -1,20 +1,21 @@
 from syringe.views.base import View
 from syringe.response import Response
+from syringe.request import Request
 
 
 class ArticleView(View):
-    def get(self, req):
+    def get(self, req: Request):
         return Response("<h1>All articles</h1>")
 
-    def post(self, req):
+    def post(self, req: Request):
         return Response("Article created!")
 
-    def patch(self, req):
+    def patch(self, req: Request):
         return Response("Article patched!")
 
 
 class SearchView(View):
-    def get(self, request):
+    def get(self, request: Request):
 
         return Response(
             f"Searching for {request.query_params.get("q")} on page {request.query_params.get("page")}"
