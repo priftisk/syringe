50091426e63c56a41569945ec5901ba5b1017900
Konstantinos Priftis <priftis.kostas1997@gmail.com>
2026-06-03 21:09:31 +0300

    Use local ip as host

--- syringe/app.py
+++ syringe/app.py
@@ -21,9 +21,21 @@ class SyringeApp:
         wrapped = apply_middlewares(handler, self.middlewares)
         return wrapped(request)
 
-    def run(self, host="127.0.0.1", port=9999):
+    def _get_local_ip(self):
+        local_hostname = socket.gethostname()
+
+        ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
+
+        filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
+
+        first_ip = filtered_ips[:1]
+
+        return first_ip[0]
+
+    def run(self, host=None, port=9999):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        host, port = host if host is not None else self._get_local_ip(), port
         server.bind((host, port))
         server.listen(5)
         print(f"Serving on http://{host}:{port} (Ctrl+C to stop)")
